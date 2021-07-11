77e6e0c63bca817562cf3a3b2a95a75b264d6ad3
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2021-07-11 15:22:01 +0900

    Specify os and version

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,10 +1,11 @@
-Greet() {
-    echo Hello "${PARAM_TO}"
+Install() {
+    echo version "${PARAM_VERSION}"
+    echo os "${PARAM_OS}"
 }
 
 # Will not run if sourced for bats-core tests.
 # View src/tests for more information.
 ORB_TEST_ENV="bats-core"
 if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
-    Greet
+    Install
 fi
