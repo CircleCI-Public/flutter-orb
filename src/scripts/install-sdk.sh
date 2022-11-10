fbc4a52d15b8f5cd71a91d2a760f3b245b392297
Jaryt <jarytbustard@gmail.com>
2022-11-10 17:24:29 -0500

    test: install location contents

--- src/scripts/install-sdk.sh
+++ src/scripts/install-sdk.sh
@@ -18,8 +18,5 @@ if [ ! -d "$INSTALL_LOCATION/flutter" ]; then
   fi
 fi
 
-# echo "$PATH"
-# echo "export PATH=$INSTALL_LOCATION/flutter/bin:$PATH">> "$BASH_ENV"
-
-# # shellcheck source=/dev/null
-# source "$BASH_ENV"
+echo "ls -la $INSTALL_LOCATION/flutter"
+echo "export PATH=$INSTALL_LOCATION/flutter/bin:$PATH" >> "$BASH_ENV"
