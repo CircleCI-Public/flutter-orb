f7bf96b790aa01bf58d1687ac25264c5c025cc4b
Jaryt <jarytbustard@gmail.com>
2022-11-10 17:00:15 -0500

    test: flutter

--- src/scripts/install-sdk.sh
+++ src/scripts/install-sdk.sh
@@ -8,7 +8,7 @@ if [ ! -d "$INSTALL_LOCATION/flutter" ]; then
     curl -o flutter_sdk.zip https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_$ORB_VAL_FLUTTER_SDK_VERSION-stable.zip
     unzip -qq flutter_sdk.zip -d "$INSTALL_LOCATION"
     rm flutter_sdk.zip
-  elif  uname -a | grep -q "Linux" ; then
+  elif uname -a | grep -q "Linux" ; then
     curl -o flutter_sdk.tar.xz https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_$ORB_VAL_FLUTTER_SDK_VERSION-stable.tar.xz
     tar xf flutter_sdk.tar.xz -C "$INSTALL_LOCATION"
     rm -f flutter_sdk.tar.xz
