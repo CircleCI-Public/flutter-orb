c11c94dc3437499f1d389aecd5463293afc3e47e
Jaryt <jarytbustard@gmail.com>
2022-11-10 17:20:41 -0500

    test: append to path outside of install commandg

--- src/scripts/install-sdk.sh
+++ src/scripts/install-sdk.sh
@@ -18,8 +18,8 @@ if [ ! -d "$INSTALL_LOCATION/flutter" ]; then
   fi
 fi
 
-echo "$PATH"
-echo "export PATH=$INSTALL_LOCATION/flutter/bin:$PATH">> "$BASH_ENV"
+# echo "$PATH"
+# echo "export PATH=$INSTALL_LOCATION/flutter/bin:$PATH">> "$BASH_ENV"
 
-# shellcheck source=/dev/null
-source "$BASH_ENV"
+# # shellcheck source=/dev/null
+# source "$BASH_ENV"
