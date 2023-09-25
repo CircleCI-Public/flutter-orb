a874dbfd973f09ace7f8abd6a25887a51a22bc26
ryanbourdais <ryanbourdais@outlook.com>
2023-09-25 16:01:15 -0500

    test: move logic into function

--- src/scripts/install-sdk.sh
+++ src/scripts/install-sdk.sh
@@ -2,31 +2,21 @@
 
 INSTALL_LOCATION=$(eval "echo $ORB_EVAL_INSTALL_LOCATION")
 
-if [ ! -d "$INSTALL_LOCATION/flutter" ]; then
-  mkdir -p "$INSTALL_LOCATION"
-  if [ "$(uname)" == 'Darwin' ]; then
-    if [ "$(uname -m)" == "arm64" ]; then
-      echo "Installing Flutter for ARM64"
-      curl -o flutter_sdk.zip https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_arm64_$ORB_VAL_FLUTTER_SDK_VERSION-stable.zip
-    else
-      curl -o flutter_sdk.zip https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_$ORB_VAL_FLUTTER_SDK_VERSION-stable.zip
-    fi
-    unzip -qq flutter_sdk.zip -d "$INSTALL_LOCATION"
-    rm flutter_sdk.zip
-  elif uname -a | grep -q "Linux" ; then
-    ENV_IS_ARM=$(! dpkg --print-architecture | grep -q arm; echo $?)
-    if [ "$ENV_IS_ARM" == "arm" ]; then
-      curl -o flutter_sdk.tar.xz https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_arm64_$ORB_VAL_FLUTTER_SDK_VERSION-stable.tar.xz
-    else
-      curl -o flutter_sdk.tar.xz https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_$ORB_VAL_FLUTTER_SDK_VERSION-stable.tar.xz
-    fi
-    tar xf flutter_sdk.tar.xz -C "$INSTALL_LOCATION"
-    rm -f flutter_sdk.tar.xz
-  else
-    echo "This platform ($(uname -a)) is not supported."
-    exit 1
-  fi
-fi
+function install_flutter() {
+  local machine=${1:-"Linux"}
+  local arch=${2:-"amd64"}
+  [[ $machine == "Darwin" ]] && uname=macos || uname=linux
+  [[ $arch == "arm64" ]] && version="flutter_${uname}_arm64" || version="flutter_${uname}"
+  [[ $uname == "linux" ]] && suffix="tar.xz" || suffix="zip"
+
+  baseurl="https://storage.googleapis.com/flutter_infra_release/releases/stable"
+
+  fullurl="$baseurl/$uname/${version}_$ORB_VAL_FLUTTER_SK_VERSION-stable.${suffix}"
+
+  curl -o "flutter_sdk.${suffix}" "$fullurl"
+}
+
+install_flutter "$(uname)" "$(uname -a)"
 
 echo "export PATH=$INSTALL_LOCATION/flutter/bin:\$PATH" >> "$BASH_ENV"
 
